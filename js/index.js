499d13883d3c6bc4b106b75da5708c6bf3c3b005
Nicholas Moore <nmoore2@gtest.northeaststate.edu>
2017-10-31 15:40:14 -0400

    navbar styling

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#homeLink").addClass("active");
+    $("#aboutLink").click(function(){
+        $("#homeLink").removeClass("active");
+        $("#aboutLink").addClass("active");
+        $("#contactLink").removeClass("active");
+        $("#projectLink").removeClass("active");
+    });
+    $("#contactLink").click(function(){
+        $("#homeLink").removeClass("active");
+        $("#aboutLink").removeClass("active");
+        $("#contactLink").addClass("active");
+        $("#projectLink").removeClass("active");
+    });
+    $("#homeLink").click(function(){
+        $("#homeLink").addClass("active");
+        $("#aboutLink").removeClass("active");
+        $("#contactLink").removeClass("active");
+        $("#projectLink").removeClass("active");
+    });
+    $("#projectLink").click(function(){
+        $("#projectLink").addClass("active");
+        $("#homeLink").removeClass("active");
+        $("#aboutLink").removeClass("active");
+        $("#contactLink").removeClass("active");
+    });
+});
