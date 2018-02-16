73b7153f57c9503532e1a788c72d7b72a551ffe4
Nick Moore <nickmoore_1@live.com>
2018-02-15 19:43:26 -0500

    Creating the main page content  

--- js/index.js
+++ js/index.js
@@ -6,30 +6,31 @@ function searchClicked() {
     menuAction.classList.toggle('active');
 };
 
-$(document).ready(function(){
-    $("#homeLink").addClass("active");
-    $("#aboutLink").click(function(){
-        $("#homeLink").removeClass("active");
-        $("#aboutLink").addClass("active");
-        $("#contactLink").removeClass("active");
-        $("#projectLink").removeClass("active");
+$(document).ready(function () {
+    $("#newSelection").hide();
+    $("#pickSelection").hide();
+    $("#newLink").click(function () {
+        $("#featuredLink").removeClass("active");
+        $("#featuredSelection").hide();
+        $("#newLink").addClass("active");
+        $("#newSelection").show();
+        $("#pickLink").removeClass("active");
+        $("#pickSelection").hide();
     });
-    $("#contactLink").click(function(){
-        $("#homeLink").removeClass("active");
-        $("#aboutLink").removeClass("active");
-        $("#contactLink").addClass("active");
-        $("#projectLink").removeClass("active");
+    $("#pickLink").click(function () {
+        $("#featuredLink").removeClass("active");
+        $("#featuredSelection").hide();
+        $("#newSelection").hide();
+        $("#newLink").removeClass("active");
+        $("#pickLink").addClass("active");
+        $("#pickSelection").show()
     });
-    $("#homeLink").click(function(){
-        $("#homeLink").addClass("active");
-        $("#aboutLink").removeClass("active");
-        $("#contactLink").removeClass("active");
-        $("#projectLink").removeClass("active");
-    });
-    $("#projectLink").click(function(){
-        $("#projectLink").addClass("active");
-        $("#homeLink").removeClass("active");
-        $("#aboutLink").removeClass("active");
-        $("#contactLink").removeClass("active");
+    $("#featuredLink").click(function () {
+        $("#featuredLink").addClass("active");
+        $("#featuredSelection").show();
+        $("#newLink").removeClass("active");
+        $("#newSelection").hide();
+        $("#pickLink").removeClass("active");
+        $("#pickSelection").hide();
     });
 });
