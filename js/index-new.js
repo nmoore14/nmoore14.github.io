6dff4271489695b408e4090aafe73265ab5cd493
Nick Moore <nickmoore_1@live.com>
2018-05-02 14:01:15 -0400

    Creating new portfolio page

---
+++ js/index-new.js
@@ -0,0 +1,5 @@
+let sideHeight = screen.height;
+let finalHeight = sideHeight + "px";
+console.log(sideHeight);
+const sideElem = document.getElementById('sideNav');
+sideElem.style.height = `${finalHeight}`;
