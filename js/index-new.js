3eaeaf1d8ec8984aa62f84569603cf2b204dab31
Nick Moore <nickmoore_1@live.com>
2018-05-02 16:33:50 -0400

    Adding javascript and more css styling

--- js/index-new.js
+++ js/index-new.js
@@ -2,4 +2,6 @@ let sideHeight = screen.height;
 let finalHeight = sideHeight + "px";
 console.log(sideHeight);
 const sideElem = document.getElementById('sideNav');
+const contentElem = document.getElementById('content');
 sideElem.style.height = `${finalHeight}`;
+contentElem.style.height = `${finalHeight}`;
