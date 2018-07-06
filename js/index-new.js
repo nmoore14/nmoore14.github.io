664e40d8e8336ea7c1879a5e4742276c79efb3f4
Nick Moore <nickmoore_1@live.com>
2018-07-06 18:27:36 -0400

    Adding menu jquery

--- js/index-new.js
+++ js/index-new.js
@@ -5,3 +5,31 @@ const sideElem = document.getElementById('sideNav');
 const contentElem = document.getElementById('content');
 sideElem.style.height = `${finalHeight}`;
 contentElem.style.height = `${finalHeight}`;
+
+$(document).ready(function () {
+    $("#aboutLink").mouseover(function () {
+        $("#aboutLink").addClass("hover");
+    });
+    $("#aboutLink").mouseout(function () {
+        $("#aboutLink").removeClass("hover");
+    });
+    $("#projectLink").mouseover(function () {
+        $("#projectLink").addClass("hover");
+    });
+    $("#projectLink").mouseout(function () {
+        $("#projectLink").removeClass("hover");
+    });
+    $("#caseStudiesLink").mouseover(function () {
+        $("#caseStudiesLink").addClass("hover");
+    });
+    $("#caseStudiesLink").mouseout(function () {
+        $("#caseStudiesLink").removeClass("hover");
+    });
+    $("#contactLink").mouseover(function () {
+        $("#contactLink").addClass("hover");
+    });
+    $("#contactLink").mouseout(function () {
+        $("#contactLink").removeClass("hover");
+    });
+});
+
