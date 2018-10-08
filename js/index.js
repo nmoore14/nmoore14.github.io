54edf603e66c27cb4d669b24eb630ecfeb452cfd
Nick Moore <nickmoore_1@live.com>
2018-10-08 15:08:48 -0400

    Working on getting links setup with vue

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ var app = new Vue({
     el: '#main',
     data: {
         message: 'Hello Vue!',
-        message2: 'This is another test'
+        message2: 'This is another test',
+        links: [
+            {
+                name: 'Home',
+                location: 'index.html'
+            },
+            {
+                name: 'About',
+                location: 'about.html'
+            }
+        ]
     }
 })
