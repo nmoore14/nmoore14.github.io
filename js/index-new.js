5c3470d20ef553531fb3062cacb84d78132a7e78
Nick Moore <nickmoore_1@live.com>
2020-08-26 14:57:20 -0400

    styling the brick layout

--- js/index-new.js
+++ js/index-new.js
@@ -1,1 +1,13 @@
+$(document).ready(function (){
+  function toggleBoxClass() {
+    $("#box-1-bg-1").toggleClass("box1-Active");
+  };
 
+  $("#box-1").mouseenter(function() {
+    toggleBoxClass()
+  })
+
+  $("#box-1").mouseleave(function() {
+    toggleBoxClass()
+  })
+});
