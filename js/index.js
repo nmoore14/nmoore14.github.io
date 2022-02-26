8b37c0f9ed33b3a9205b4cf41f12ecf8b975a50c
Nick Moore <nmoore@foreup.com>
2022-02-26 00:13:07 -0500

    Adding the site info modal and its functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
-    console.log('This is a test');
+    $(".js-info-modal-toggle").click(() => {
+        $(".js-modal").toggleClass('modal-active');
+    });
+
+    $(".js-modal").click(() => {
+        $(".js-modal").toggleClass('modal-active');
+    });
+
+    $(".js-modal-body").click((e) => {
+        e.stopPropagation();
+    });
+
+    $(".js-modal-close").click(() => {
+        $(".js-modal").removeClass('modal-active');
+    });
 });
