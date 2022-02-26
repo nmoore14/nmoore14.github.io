af0fca4fd75491e6636f0982f1abed953a133894
Nick Moore <nmoore@foreup.com>
2022-02-25 23:12:17 -0500

    Adding and updating to a new style

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-var app = new Vue({
-    el: '#main',
-    data: {
-        pro: [
-            {
-                name: '* MAMP Localhost Customization',
-                location: 'https://github.com/nmoore14/MAMP_Localhost_Customization',
-                source: 'https://github.com/nmoore14/MAMP_Localhost_Customization',
-                completed: false
-            }
-        ],
-        fun: [
-            {
-                name: 'Guess-A-Number V1',
-                location: 'projects/fun/Guess-A-Number_v1/index.html',
-                source: 'https://github.com/nmoore14/Fall_2018_School/tree/master/WEBT%202300%20-%20Client%20Side%20Programming/Projects/NickMoore_Exam1',
-                completed: true
-            },
-            {
-                name: 'Monster Slayer',
-                location: 'projects/fun/Monster-Slayer/index.html',
-                source: 'https://jsfiddle.net/nmoore14/ovLm2rg1/136/',
-                completed: true
-            }
-        ]
-    },
-    mounted:
-        function() {
-            alert("This is a work in progress.");
-        }
-
-})
+$( document ).ready(function() {
+    console.log('This is a test');
+});
